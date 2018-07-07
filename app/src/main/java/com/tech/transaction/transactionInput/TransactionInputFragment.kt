a707659467de790d83c0a9cc21200fb7da333fc1
wong0 <genewong2016@gmail.com>
2018-07-07 15:53:08 +0800

    Add TransactionInputFragment

---
+++ app/src/main/java/com/tech/transaction/transactionInput/TransactionInputFragment.kt
@@ -0,0 +1,25 @@
+package com.tech.transaction.transactionInput
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tech.transaction.R
+import kotlinx.android.synthetic.main.fragment_transaction.*
+
+class TransactionInputFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSubmit.setOnClickListener {
+        //        etAmount.text.toString()
+        }
+    }
+}
