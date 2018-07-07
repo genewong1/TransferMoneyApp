f206b2590d020ceda65c77770d82cb70e5079495
wong0 <genewong2016@gmail.com>
2018-07-07 15:50:17 +0800

    Add TransactionInputFragment

--- app/src/main/java/com/tech/transaction/MainActivity.kt
+++ app/src/main/java/com/tech/transaction/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tech.transaction
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.tech.transaction.transactionInput.TransactionInputFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
