a3790249d72cf13fcb0a5c9a0f72ea8c1b10348e
wong0 <genewong2016@gmail.com>
2018-07-09 00:11:26 +0800

    injection for TransactionInputInteractorImpl

--- app/src/main/java/com/tech/transaction/transactionInput/contract/TransactionInputInteractorImpl.kt
+++ app/src/main/java/com/tech/transaction/transactionInput/contract/TransactionInputInteractorImpl.kt
@@ -1,8 +1,20 @@
 package com.tech.transaction.transactionInput.contract
 
 import java.math.BigDecimal
+import javax.inject.Inject
 
-class TransactionInputInteractorImpl(presenter: TransactionInputContract.Presenter) : TransactionInputContract.Interactor {
+class TransactionInputInteractorImpl : TransactionInputContract.Interactor {
+    private var output: TransactionInputContract.InteractorOutput
+
+    @Inject
+    constructor (output: TransactionInputContract.InteractorOutput) {
+        this.output = output
+    }
+
+    init {
+
+    }
+
     override fun initiateTransaction(amount: BigDecimal) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
