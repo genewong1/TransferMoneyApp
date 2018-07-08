0a126c948640bb0a1194f5c3432d94d990ee6733
wong0 <genewong2016@gmail.com>
2018-07-08 15:59:27 +0800

    Add presenter unit test stubs

--- app/src/test/java/com/tech/transaction/transactionInput/contract/TransactionInputPresenterUnitTest.kt
+++ app/src/test/java/com/tech/transaction/transactionInput/contract/TransactionInputPresenterUnitTest.kt
@@ -1,5 +1,33 @@
 package com.tech.transaction.transactionInput.contract
 
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
 class TransactionInputPresenterUnitTest {
+    @Mock
+    private lateinit var view : TransactionInputFragmentContract.View
+
+    @Mock
+    private lateinit var interactor : TransactionInputFragmentContract.Interactor
+
+    private lateinit var presenter : TransactionInputFragmentContract.Presenter
+
+    @Before
+    fun setup() {
+        // When use @Mock annotation, we need to trigger the creation of annotated objects by calling iniMocks() method.
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun onBtnSubmit_CallsInitiateTransaction() {
+        //todo
+    }
+
+    @Test
+    fun onTransferRequestComplete_TransitionsToResultFragment() {
+        //todo
+    }
 
 }
