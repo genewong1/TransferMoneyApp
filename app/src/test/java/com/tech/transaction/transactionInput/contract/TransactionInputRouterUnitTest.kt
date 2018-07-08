e380f55f119a0d2bebafb91db9f9f0c6154a0403
wong0 <genewong2016@gmail.com>
2018-07-08 15:59:40 +0800

    Add router unit test stubs

--- app/src/test/java/com/tech/transaction/transactionInput/contract/TransactionInputRouterUnitTest.kt
+++ app/src/test/java/com/tech/transaction/transactionInput/contract/TransactionInputRouterUnitTest.kt
@@ -1,5 +1,10 @@
 package com.tech.transaction.transactionInput.contract
 
-class TransactionInputRouterUnitTest {
+import org.junit.Test
 
+class TransactionInputRouterUnitTest {
+    @Test
+    fun goToTransactionResult() {
+        //Given status: TransactionStatus, go to transaction result.
+    }
 }
