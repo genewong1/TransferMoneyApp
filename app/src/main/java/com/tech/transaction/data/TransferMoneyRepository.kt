8b973816c8481082e1b4200772e59ae8d737f71a
eugenewong <genewong2016@gmail.com>
2018-07-09 22:19:17 +0800

    Add TransferMoneyRepository

---
+++ app/src/main/java/com/tech/transaction/data/TransferMoneyRepository.kt
@@ -0,0 +1,26 @@
+package com.tech.transaction.data
+
+import com.tech.transaction.data.dto.TransferMoneyOutDtoFactory
+import com.tech.transaction.entities.TransactionStatus.TransferMoneyInDto
+import com.tech.transaction.transactionInput.api.TransferMoneyService
+import rx.Observable
+import java.math.BigDecimal
+import javax.inject.Inject
+
+
+
+class TransferMoneyRepository {
+    private var service: TransferMoneyService
+
+    @Inject
+    constructor(service: TransferMoneyService) {
+        this.service = service
+    }
+
+    fun transferMoney(amount: BigDecimal): Observable<TransferMoneyInDto> {
+        val factory = TransferMoneyOutDtoFactory()
+        val transferMoneyOutDto = factory.build(amount = amount)
+        return service.sendMoney(transferMoneyOutDto = transferMoneyOutDto)
+    }
+
+}
