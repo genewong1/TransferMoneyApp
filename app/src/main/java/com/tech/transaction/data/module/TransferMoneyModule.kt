40efb90063529e409aead3a9e073d01355c0d54a
wong0 <genewong2016@gmail.com>
2018-07-09 18:31:59 +0800

    add TransferMoneyModule

---
+++ app/src/main/java/com/tech/transaction/data/module/TransferMoneyModule.kt
@@ -0,0 +1,45 @@
+package com.tech.transaction.data.module
+
+import android.app.Application
+import com.tech.transaction.transactionInput.api.TransferMoneyService
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+class TransferMoneyModule {
+    var application: Application? = null
+
+    constructor(application: Application) {
+        this.application = application
+    }
+
+    @Provides
+    fun provideTransferMoneyService(retrofit: Retrofit) : TransferMoneyService {
+        return retrofit.create(TransferMoneyService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val BASE_URL = ""
+
+        return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                //converts Retrofit response into Observable
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient().newBuilder().build()
+    }
+
+}
