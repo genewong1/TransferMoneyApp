bb91f6276e8f81ec4db12e1d8078a1f135c7b4fc
eugenewong <genewong2016@gmail.com>
2018-07-09 18:56:31 +0800

    add TransactionStatus.kt

--- app/src/main/java/com/tech/transaction/entities/TransactionStatus/TransactionStatus.kt
+++ app/src/main/java/com/tech/transaction/entities/TransactionStatus/TransactionStatus.kt
@@ -1,5 +1,28 @@
 package com.tech.transaction.entities.TransactionStatus
 
-class TransactionStatus {
-    //TODO
+import com.google.gson.annotations.SerializedName
+
+class TransactionStatus(
+        success: Boolean?,
+        amount: Int?,
+        statusDescription: String?
+) {
+    @SerializedName("success")
+    var success: Boolean? = null
+
+    @SerializedName("amount")
+    var amount: Int? = null
+
+    @SerializedName("status_description")
+    var statusDescription: String? = null
+
+    init {
+        this.success = success
+        this.amount = amount
+        this.statusDescription = statusDescription
+    }
+
+    override fun toString(): String {
+        return "TransactionStatus(success=$success, amount=$amount, statusDescription=$statusDescription)"
+    }
 }
