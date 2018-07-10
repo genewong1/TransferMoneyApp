d700682d111753d3cb273a5513d2ef44ddffc53c
eugenewong <genewong2016@gmail.com>
2018-07-10 22:38:25 +0800

    simplify instantiation with newInstance call.

--- app/src/main/java/com/tech/transaction/transactionInput/contract/TransactionInputRouterImpl.kt
+++ app/src/main/java/com/tech/transaction/transactionInput/contract/TransactionInputRouterImpl.kt
@@ -1,6 +1,5 @@
 package com.tech.transaction.transactionInput.contract
 
-import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import com.tech.transaction.R
 import com.tech.transaction.entities.TransactionStatus.TransactionStatus
@@ -11,15 +10,13 @@ class TransactionInputRouterImpl(
 ) : TransactionInputContract.Router {
 
     override fun goToTransactionResult(transactionStatus: TransactionStatus) {
-        val transactionResultFragment = TransactionResultFragment()
-
-        val bundle = Bundle()
-        bundle.putParcelable(TransactionResultFragment.KEY_TRANSACTION_STATUS, transactionStatus)
-        transactionResultFragment.arguments = bundle
-
         this.fragmentManager
             .beginTransaction()
-            .replace(R.id.fragmentContainer, transactionResultFragment)
+            .addToBackStack("transaction")
+            .replace(
+                    R.id.fragmentContainer,
+                    TransactionResultFragment.newInstance(transactionStatus)
+            )
             .commit()
     }
 }
