0053338b357dc7169daf60fabf0342254a9f7fa5
eugenewong <genewong2016@gmail.com>
2018-07-10 10:47:10 +0800

    add KEY_TRANSACTION_STATUS, transactionStatus.

--- app/src/main/java/com/tech/transaction/transactionResult/TransactionResultFragment.kt
+++ app/src/main/java/com/tech/transaction/transactionResult/TransactionResultFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tech.transaction.R
+import com.tech.transaction.entities.TransactionStatus.TransactionStatus
 
 class TransactionResultFragment  : Fragment() {
+
+    private val transactionStatus = this.arguments?.getParcelable<TransactionStatus>(KEY_TRANSACTION_STATUS)
+                                ?: throw IllegalArgumentException("Parcelable KEY_TRANSACTION_STATUS expected")
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -18,4 +23,7 @@ class TransactionResultFragment  : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    companion object {
+        const val KEY_TRANSACTION_STATUS = "TRANSACTION_STATUS"
+    }
 }
