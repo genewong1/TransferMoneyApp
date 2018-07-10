fc530a7505cf31eeefa02eb08c825dddc01cf31c
eugenewong <genewong2016@gmail.com>
2018-07-10 22:03:23 +0800

    remove param.

--- app/src/main/java/com/tech/transaction/data/dto/in/TransferMoneyInDto.kt
+++ app/src/main/java/com/tech/transaction/data/dto/in/TransferMoneyInDto.kt
@@ -3,21 +3,16 @@ package com.tech.transaction.data.dto.`in`
 import com.google.gson.annotations.SerializedName
 
 class TransferMoneyInDto(
-        success: Boolean?,
-        amount: Int?
+        success: Boolean?
 ) {
     @SerializedName("success")
     var success: Boolean? = null
 
-    @SerializedName("amount")
-    var amount: Int? = null
-
     init {
         this.success = success
-        this.amount = amount
     }
 
     override fun toString(): String {
-        return "TransferMoneyInDto(success=$success, amount=$amount)"
+        return "TransferMoneyInDto(success=$success)"
     }
 }
