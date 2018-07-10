5c0a0e493e361054e63643e6656519986ce6544c
eugenewong <genewong2016@gmail.com>
2018-07-10 17:15:11 +0800

    Add goToTransactionResult.

--- app/src/main/java/com/tech/transaction/transactionInput/contract/TransactionInputRouterImpl.kt
+++ app/src/main/java/com/tech/transaction/transactionInput/contract/TransactionInputRouterImpl.kt
@@ -1,10 +1,25 @@
 package com.tech.transaction.transactionInput.contract
 
+import android.os.Bundle
 import android.support.v4.app.FragmentManager
+import com.tech.transaction.R
 import com.tech.transaction.entities.TransactionStatus.TransactionStatus
+import com.tech.transaction.transactionResult.TransactionResultFragment
 
-class TransactionInputRouterImpl(fragmentManager: FragmentManager) : TransactionInputContract.Router {
+class TransactionInputRouterImpl(
+    private val fragmentManager: FragmentManager
+) : TransactionInputContract.Router {
+
     override fun goToTransactionResult(transactionStatus: TransactionStatus) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val transactionResultFragment = TransactionResultFragment()
+
+        val bundle = Bundle()
+        bundle.putParcelable(TransactionResultFragment.KEY_TRANSACTION_STATUS, transactionStatus)
+        transactionResultFragment.arguments = bundle
+
+        this.fragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, transactionResultFragment)
+            .commit()
     }
 }
