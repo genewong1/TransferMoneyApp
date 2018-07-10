c25e7e8b25ea6657893b9961967593abbf0d48d2
eugenewong <genewong2016@gmail.com>
2018-07-10 22:17:09 +0800

    add newInstance(transactionStatus). add onViewCreated(...) - extract transactionStatus. inject presenter.

--- app/src/main/java/com/tech/transaction/transactionResult/TransactionResultFragment.kt
+++ app/src/main/java/com/tech/transaction/transactionResult/TransactionResultFragment.kt
@@ -1,17 +1,23 @@
 package com.tech.transaction.transactionResult
 
 import android.os.Bundle
+import android.support.annotation.StringRes
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tech.transaction.R
 import com.tech.transaction.entities.TransactionStatus.TransactionStatus
+import com.tech.transaction.transactionResult.component.DaggerTransactionResultComponent
+import com.tech.transaction.transactionResult.contract.TransactionResultContract
+import com.tech.transaction.transactionResult.module.TransactionResultPresenterModule
+import kotlinx.android.synthetic.main.fragment_transaction_result.*
+import javax.inject.Inject
 
-class TransactionResultFragment  : Fragment() {
+class TransactionResultFragment : Fragment(), TransactionResultContract.View {
 
-    private val transactionStatus = this.arguments?.getParcelable<TransactionStatus>(KEY_TRANSACTION_STATUS)
-                                ?: throw IllegalArgumentException("Parcelable KEY_TRANSACTION_STATUS expected")
+    @Inject
+    lateinit var presenter: TransactionResultContract.Presenter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -21,9 +27,35 @@ class TransactionResultFragment  : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val transactionStatus = this.arguments?.getParcelable<TransactionStatus>(KEY_TRANSACTION_STATUS)
+                ?: throw IllegalArgumentException("Parcelable KEY_TRANSACTION_STATUS expected")
+
+        DaggerTransactionResultComponent.builder()
+                .transactionResultPresenterModule(
+                        TransactionResultPresenterModule(this)
+                )
+                .build()
+                .inject(this)
+
+        presenter.onViewCreatedAndStatusReceived(transactionStatus)
+    }
+
+    override fun showStatus(@StringRes statusStringResId: Int) {
+        tvTransactionResult.setText(statusStringResId)
     }
 
     companion object {
         const val KEY_TRANSACTION_STATUS = "TRANSACTION_STATUS"
+
+        fun newInstance(transactionStatus: TransactionStatus) : TransactionResultFragment {
+            val transactionResultFragment = TransactionResultFragment()
+
+            val bundle = Bundle()
+            bundle.putParcelable(TransactionResultFragment.KEY_TRANSACTION_STATUS, transactionStatus)
+            transactionResultFragment.arguments = bundle
+
+            return transactionResultFragment
+        }
     }
 }
