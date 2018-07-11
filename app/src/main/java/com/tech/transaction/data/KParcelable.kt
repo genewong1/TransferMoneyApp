6c575abc4ee0f824ebe18b43f5c3f3c2fee026ce
eugenewong <genewong2016@gmail.com>
2018-07-11 17:40:11 +0800

    remove unneeded inlines.

--- app/src/main/java/com/tech/transaction/data/KParcelable.kt
+++ app/src/main/java/com/tech/transaction/data/KParcelable.kt
@@ -20,30 +20,11 @@ inline fun <reified T> parcelableCreator(
             override fun newArray(size: Int) = arrayOfNulls<T>(size)
         }
 
-
-inline fun <reified T> parcelableClassLoaderCreator(
-        crossinline create: (Parcel, ClassLoader) -> T) =
-        object : Parcelable.ClassLoaderCreator<T> {
-            override fun createFromParcel(source: Parcel, loader: ClassLoader) =
-                    create(source, loader)
-
-            override fun createFromParcel(source: Parcel) =
-                    createFromParcel(source, T::class.java.classLoader)
-
-            override fun newArray(size: Int) = arrayOfNulls<T>(size)
-        }
-
 // Parcel extensions
 
-inline fun Parcel.readBoolean() = readInt() != 0
+fun Parcel.readBoolean() = readInt() != 0
 
-inline fun Parcel.writeBoolean(value: Boolean) = writeInt(if (value) 1 else 0)
-
-inline fun <reified T : Enum<T>> Parcel.readEnum() =
-        readInt().let { if (it >= 0) enumValues<T>()[it] else null }
-
-inline fun <T : Enum<T>> Parcel.writeEnum(value: T?) =
-        writeInt(value?.ordinal ?: -1)
+fun Parcel.writeBoolean(value: Boolean) = writeInt(if (value) 1 else 0)
 
 inline fun <T> Parcel.readNullable(reader: () -> T) =
         if (readInt() != 0) reader() else null
@@ -56,12 +37,6 @@ inline fun <T> Parcel.writeNullable(value: T?, writer: (T) -> Unit) {
         writeInt(0)
     }
 }
-
-fun Parcel.readBigInteger() =
-        readNullable { BigInteger(createByteArray()) }
-
-fun Parcel.writeBigInteger(value: BigInteger?) =
-        writeNullable(value) { writeByteArray(it.toByteArray()) }
 
 fun Parcel.readBigDecimal() =
         readNullable { BigDecimal(BigInteger(createByteArray()), readInt()) }
