e5d1a5fb0189e594f5c92deee3b36c613f778931
eugenewong <genewong2016@gmail.com>
2018-07-11 23:37:20 +0800

    Add tests to TransactionInputPresenterUnitTest

--- app/src/test/java/com/tech/transaction/transferMoneyInput/contract/TransactionInputPresenterUnitTest.kt
+++ app/src/test/java/com/tech/transaction/transferMoneyInput/contract/TransactionInputPresenterUnitTest.kt
@@ -1,16 +1,23 @@
 package com.tech.transaction.transferMoneyInput.contract
 
+import com.tech.transaction.entities.TransferMoneyStatus.TransferMoneyStatus
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
+import java.math.BigDecimal
+
 
 class TransactionInputPresenterUnitTest {
     @Mock
     private lateinit var view : InputContract.View
 
     @Mock
-    private lateinit var interactor : InputContract.Interactor
+    lateinit var interactor : InputContract.Interactor
+
+    @Mock
+    private lateinit var router: InputContract.Router
 
     private lateinit var presenter : InputContract.Presenter
 
@@ -18,16 +25,81 @@ class TransactionInputPresenterUnitTest {
     fun setup() {
         // When use @Mock annotation, we need to trigger the creation of annotated objects by calling iniMocks() method.
         MockitoAnnotations.initMocks(this)
+
+        presenter = InputPresenterImpl(view, router)
+
     }
 
     @Test
     fun onBtnSubmit_CallsInitiateTransaction() {
-        //todo
+        val strAmount = "201"
+        val bdAmount = BigDecimal("201")
+
+        presenter = InputPresenterImpl(view, router, interactor)
+
+        presenter.onBtnSubmit(strAmount)
+
+        verify(interactor).initiateTransaction(bdAmount)
+
     }
 
     @Test
     fun onTransferRequestComplete_TransitionsToResultFragment() {
-        //todo
+        val strAmount = "2331"
+        val bdAmount = BigDecimal(strAmount)
+
+        presenter = InputPresenterImpl(view, router)
+
+        val transferMoneyStatus = TransferMoneyStatus(true, bdAmount)
+        presenter.onTransferRequestComplete(transferMoneyStatus = transferMoneyStatus)
+        verify(router).goToTransactionResult(transferMoneyStatus)
+
+        val transferMoneyStatusSuccessFalse = TransferMoneyStatus(false, bdAmount)
+        presenter.onTransferRequestComplete(transferMoneyStatus = transferMoneyStatusSuccessFalse)
+        verify(router).goToTransactionResult(transferMoneyStatusSuccessFalse)
+
+    }
+
+    @Test
+    fun onTransferRequestError_TransitionsToResultFragment() {
+        val strAmount = "23311"
+        val bdAmount = BigDecimal(strAmount)
+
+        presenter = InputPresenterImpl(view, router)
+
+        val transferMoneyStatus = TransferMoneyStatus(true, bdAmount)
+        presenter.onTransferRequestError(transferMoneyStatus = transferMoneyStatus)
+        verify(router).goToTransactionResult(transferMoneyStatus)
+
+        val transferMoneyStatusSuccessFalse = TransferMoneyStatus(false, bdAmount)
+        presenter.onTransferRequestError(transferMoneyStatus = transferMoneyStatusSuccessFalse)
+        verify(router).goToTransactionResult(transferMoneyStatusSuccessFalse)
+    }
+
+    @Test
+    fun onAmountInputValid_CallsEnableBtnSubmit() {
+        presenter = InputPresenterImpl(view, router)
+
+        presenter.onAmountInputValid()
+        verify(view).enableBtnSubmit(true)
+    }
+
+    @Test
+    fun onAmountInputInvalid_CallsEnableBtnSubmit() {
+        presenter = InputPresenterImpl(view, router)
+
+        presenter.onAmountInputInvalid()
+        verify(view).enableBtnSubmit(false)
+    }
+
+    @Test
+    fun onEtAmountFieldChanged_GivenAmount() {
+        val amount = "201"
+
+        presenter = InputPresenterImpl(view, router, interactor)
+
+        presenter.onEtAmountFieldChanged(amount)
+        verify(interactor).isAmountInputValid(amount)
     }
 
 }
